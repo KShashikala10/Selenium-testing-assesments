d96a5c97f81ef8c8060fcf9316901adaccab0e7a
K Shashikala <167070344+KShashikala10@users.noreply.github.com>
2026-09-17 09:07:33 +0530

    Add keywordsauce.java with main execution flow

---
+++ keywordsauce.java
@@ -0,0 +1,20 @@
+package KeywordFrameworkImplementation;
+
+import KeyWordDriven.KeywordExecutors;
+
+public class keywordsauce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		KeywordExecutors e = new KeywordExecutors();
+		
+		e.executors("LAUNCH_BROWSER");
+		e.executors("OPNE_URL");
+		e.executors("USER_NAME");
+		e.executors("PASSWORD");
+		e.executors("LOGIN");
+		e.executors("CLOSE_BROWSER");
+
+	}
+
+}
