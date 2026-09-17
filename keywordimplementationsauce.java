9cc4b7d349cb63882936199403fa6699f4b6124c
K Shashikala <167070344+KShashikala10@users.noreply.github.com>
2026-09-17 09:06:35 +0530

    Add keyword implementation for Sauce Labs login

---
+++ keywordimplementationsauce.java
@@ -0,0 +1,44 @@
+package KeyWordDriven;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class keywordimplemenatationsauce {
+
+
+
+		
+		WebDriver driver;
+		public void launchBrowser()
+		{
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			
+		}
+		public void openurl() {
+			driver.get("https://demowebshop.tricentis.com");
+		}
+
+		public void username()
+		{
+			driver.findElement(By.id("user_name")).sendKeys("standard_user");
+		}
+		public void password()
+		{
+			driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		}
+		public void login()
+		{
+			driver.findElement(By.id("login-button")).click();
+		}
+		public void closebrowser()
+		{
+			driver.quit();
+		}
+	}
+
+
