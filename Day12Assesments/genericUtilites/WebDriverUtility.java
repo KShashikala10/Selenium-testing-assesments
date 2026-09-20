6f18e2daa041f3d39f596855c0b575f3b43c37b9
K Shashikala <167070344+KShashikala10@users.noreply.github.com>
2026-09-20 20:38:27 +0530

    Add WebDriverUtility for element interactions

---
+++ Day12Assesments/genericUtilites/WebDriverUtility.java
@@ -0,0 +1,29 @@
+package genericUtilities;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebDriverUtility {
+
+    public void waitForElement(WebDriver driver, WebElement element) {
+
+        WebDriverWait wait = new WebDriverWait(
+                driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void click(WebDriver driver, WebElement element) {
+
+        WebDriverWait wait = new WebDriverWait(
+                driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+
+        element.click();
+    }
+}
